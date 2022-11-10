75b9199fff3a33e5af783644a1087a059e6fd145
Mohammed Saad Tebaa <mohammedsaadtebaa09@gmail.com>
2022-11-10 15:53:09 +0100

    FrontEnd for Worker Profil For Client Activity.

---
+++ app/src/main/java/com/example/bricoli/WorkerProfilForClientActivity.java
@@ -0,0 +1,14 @@
+package com.example.bricoli;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WorkerProfilForClientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_worker_profil_for_client);
+    }
+}
