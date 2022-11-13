1395b7dd6797052221e7ff6dddb4f285e0e024b6
SAZING <you@example.com>
2022-11-13 19:16:46 +0100

    dropdown menu services (in home fragment) treatment

---
+++ app/src/main/java/com/example/bricoli/HomeFragment.java
@@ -0,0 +1,90 @@
+package com.example.bricoli;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import com.example.bricoli.databinding.FragmentHomeBinding;
+import com.google.android.material.textfield.TextInputLayout;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+
+    FragmentHomeBinding binding;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment HomeFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static HomeFragment newInstance(String param1, String param2) {
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding=FragmentHomeBinding.inflate(getLayoutInflater());
+        return binding.autoCompleteTxt.getRootView();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextInputLayout textInputLayout;
+        AutoCompleteTextView autoCompleteTextView;
+        ArrayAdapter<String> adapterItems;
+
+        //spinner123
+        textInputLayout=(TextInputLayout) getView().findViewById(R.id.textInputLayout);
+        autoCompleteTextView=(AutoCompleteTextView) getView().findViewById(R.id.autoCompleteTxt);
+
+        String[] items = {"Tech","Organisateur de fêtes", "Plombier"};
+        ArrayAdapter<String> itemAdapter=new ArrayAdapter<>(getView().getContext(), R.layout.list_item, items);
+        autoCompleteTextView.setAdapter(itemAdapter);
+    }
+}
