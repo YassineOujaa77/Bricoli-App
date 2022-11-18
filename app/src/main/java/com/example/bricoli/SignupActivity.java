01e120e328fc04e0d3f00c9c2ea044e66630a48f
BOUSSERRHINE Ziyad <ziyad.code@gmail.com>
2022-11-18 16:49:36 +0100

    Add links 1.0

--- app/src/main/java/com/example/bricoli/SignupActivity.java
+++ app/src/main/java/com/example/bricoli/SignupActivity.java
@@ -1,8 +1,8 @@
 package com.example.bricoli;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
 
