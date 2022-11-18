a3018efec313e576c5fb19df2ecca358500b4514
SAZING <you@example.com>
2022-11-18 17:00:00 +0100

    add drop down menu list for services categories

--- app/src/main/java/com/example/bricoli/ClientHomeActivity.java
+++ app/src/main/java/com/example/bricoli/ClientHomeActivity.java
@@ -8,12 +8,18 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.example.bricoli.databinding.ActivityClientHomeBinding;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class ClientHomeActivity extends AppCompatActivity {
+
+    private ActivityClientHomeBinding binding;
 
     private Button button;
     Fragment fragment;
@@ -21,6 +27,7 @@ public class ClientHomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityClientHomeBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_client_home);
 
         // initialize
@@ -29,6 +36,7 @@ public class ClientHomeActivity extends AppCompatActivity {
         // set Home Selected
         bottomNavigationView.setSelectedItemId(R.id.home);
 
+
         // item from menu selected listener
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -36,22 +44,22 @@ public class ClientHomeActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
                     case R.id.home:
                         return true;
-
-
                 }
-
-
                 return false;
             }
         });
 
-        button = (Button) findViewById(R.id.addButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openHomeBidsActivity();
-            }
-        });
+        TextInputLayout textInputLayout;
+        AutoCompleteTextView autoCompleteTextView;
+        ArrayAdapter<String> adapterItems;
+
+        //spinner123
+        textInputLayout=findViewById(R.id.textInputLayout);
+        autoCompleteTextView=findViewById(R.id.autoCompleteTxt);
+
+        String[] items = {"Tech","Organisateur de fêtes", "Plombier"};
+        ArrayAdapter<String> itemAdapter=new ArrayAdapter<>(ClientHomeActivity.this, R.layout.list_item_for_home_client, items);
+        autoCompleteTextView.setAdapter(itemAdapter);
     }
 
     public void openHomeBidsActivity() {
