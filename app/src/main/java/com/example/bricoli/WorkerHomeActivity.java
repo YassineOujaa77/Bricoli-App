7212cf3201494d43327c00d4d3a46e6fc8d9c892
YassineOujaa77 <yassineoujaa42@gmail.com>
2022-11-18 15:33:31 +0100

    add bottom menu

--- app/src/main/java/com/example/bricoli/WorkerHomeActivity.java
+++ app/src/main/java/com/example/bricoli/WorkerHomeActivity.java
@@ -1,9 +1,14 @@
 package com.example.bricoli;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ListView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
@@ -17,6 +22,30 @@ public class WorkerHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_worker_home);
+
+        // initialize
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        // item from menu selected listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.home:
+                        return true;
+
+
+                }
+
+
+                return false;
+            }
+        });
+
         ButterKnife.bind(this);
         ArrayList<Annoucement> annoucements = new ArrayList<>();
         annoucements.add(new Annoucement("Full Name1","Rabat","2.5 (500)","300DH","2 jrs","900m","Je suis à la recherche d'un plombier...........",R.drawable.photo));
