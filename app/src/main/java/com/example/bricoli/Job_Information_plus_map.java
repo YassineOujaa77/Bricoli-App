5f40c872a5bb4d89afebc474ddd4554a9391954a
chaymaa elharti <elhartichaymaa71@gmail.com>
2022-11-18 15:07:15 +0100

    2ndCommit

--- app/src/main/java/com/example/bricoli/Job_Information_plus_map.java
+++ app/src/main/java/com/example/bricoli/Job_Information_plus_map.java
@@ -3,18 +3,14 @@ package com.example.bricoli;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
-public class workerinformations extends AppCompatActivity {
+public class Job_Information_plus_map extends AppCompatActivity {
 
 
     private ImageButton imageButton;
