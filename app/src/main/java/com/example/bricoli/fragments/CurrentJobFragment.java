fc0a43cee9b89e9877612c5d1b8f0fddc1535e14
YassineOujaa77 <yassineoujaa42@gmail.com>
2022-11-26 18:16:59 +0100

    fix typo

--- app/src/main/java/com/example/bricoli/fragments/CurrentJobFragment.java
+++ app/src/main/java/com/example/bricoli/fragments/CurrentJobFragment.java
@@ -61,6 +61,6 @@ public class CurrentJobFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_current_job2, container, false);
+        return inflater.inflate(R.layout.fragment_current_job, container, false);
     }
 }
