046902099e63ef18daeb20bd684524146668d8d7
Soukaina Elaitari <soukaina.elaitari312@gmail.com>
2022-11-29 02:51:24 +0100

    Account settings

---
+++ app/src/main/java/com/example/bricoli/activities/SettingActivity.java
@@ -0,0 +1,16 @@
+package com.example.bricoli.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bricoli.R;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
