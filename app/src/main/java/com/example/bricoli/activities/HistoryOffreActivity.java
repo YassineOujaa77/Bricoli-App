33467df1bb3cf12a6dbfea289ae6e9109eee42ee
YassineOujaa77 <yassineoujaa42@gmail.com>
2022-12-03 22:51:11 +0100

    add navigation bar

--- app/src/main/java/com/example/bricoli/activities/HistoryOffreActivity.java
+++ app/src/main/java/com/example/bricoli/activities/HistoryOffreActivity.java
@@ -1,13 +1,18 @@
 package com.example.bricoli.activities;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.bricoli.R;
 import com.example.bricoli.adapters.HistoryAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HistoryOffreActivity extends AppCompatActivity {
 
@@ -27,6 +32,37 @@ public class HistoryOffreActivity extends AppCompatActivity {
         String descriptions[]={"je suis entrain de chercher un plombier pour réparation d’une ....","je suis entrain de chercher un plombier pour réparation d’une ....","je suis entrain de chercher un plombier pour réparation d’une ...."};
         HistoryAdapter historyAdapter=new HistoryAdapter(getApplicationContext(),images,nomComple,ville,notes,dates,descriptions);
         historylist.setAdapter(historyAdapter);
+
+
+        // initialize
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.history);
+
+
+        // item from menu selected listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), ClientHomeActivity.class));
+                        return true;
+                    case R.id.current:
+                        startActivity(new Intent(getApplicationContext(), PostsActuelActivity.class));
+                        return true;
+                    case R.id.history:
+                        return true;
+                    case R.id.profile:
+                        startActivity(new Intent(getApplicationContext(), SettingActivity.class));
+                        return true;
+                    default:
+                        return false;
+
+                }
+            }
+        });
 
 
 
