6af4ae0dbca04d7cb12a218b03a763cdc63932c7
SAZING <you@example.com>
2023-01-05 23:46:37 +0100

    add offer api

--- app/src/main/java/com/example/bricoli/activities/ClientHomeActivity.java
+++ app/src/main/java/com/example/bricoli/activities/ClientHomeActivity.java
@@ -111,8 +111,8 @@ public class ClientHomeActivity extends AppCompatActivity {
         call.enqueue(new Callback<Offer>() {
             @Override
             public void onResponse(Call<Offer> call, Response<Offer> response) {
-                Toast.makeText(ClientHomeActivity.this, String.valueOf(response.code()), Toast.LENGTH_SHORT).show();
-                //openHomeBidsActivity();
+                //Toast.makeText(ClientHomeActivity.this, String.valueOf(response.code()), Toast.LENGTH_SHORT).show();
+                openHomeBidsActivity();
             }
             @Override
             public void onFailure(Call<Offer> call, Throwable t) {
