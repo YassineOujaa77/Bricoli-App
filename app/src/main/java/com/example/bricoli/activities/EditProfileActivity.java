fe1c4662f8f30e250c34c39360c22514aaab14f6
YassineOujaa77 <yassineoujaa42@gmail.com>
2023-01-08 17:43:38 +0100

    Edit profile

--- app/src/main/java/com/example/bricoli/activities/EditProfileActivity.java
+++ app/src/main/java/com/example/bricoli/activities/EditProfileActivity.java
@@ -1,24 +1,51 @@
 package com.example.bricoli.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.bricoli.R;
+import com.example.bricoli.retrofit.RetrofitService;
+import com.example.bricoli.util.RealPathUtil;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    ImageButton imagePicker;
+    String path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
+         imagePicker = findViewById(R.id.image_picker);
 
-        ImageButton imagePicker = findViewById(R.id.image_picker);
         Button save = findViewById(R.id.save);
 
 
@@ -26,17 +53,61 @@ public class EditProfileActivity extends AppCompatActivity {
         imagePicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                startActivityForResult(intent,1);
+                if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_PICK);
+                    startActivityForResult(intent, 10);
+                } else {
+                    ActivityCompat.requestPermissions(EditProfileActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+                }
+
             }
+
         });
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // waiting for settings activity name
+
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 10 && resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getData();
+            Context context = EditProfileActivity.this;
+            path = RealPathUtil.getRealPath(context, uri);
+            Bitmap bitmap = BitmapFactory.decodeFile(path);
+            Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap,200,200,true);
+            imagePicker.setImageBitmap(resizedBitmap);
+
+        }
+    }
+
+
+    protected void editProfile(String fullname , String phone , String address){
+        RetrofitService retrofitService = new RetrofitService();
+
+
+        File file = new File(path);
+        RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+
+        MultipartBody.Part body = MultipartBody.Part.createFormData("image", file.getName(), requestFile);
+
+        RequestBody fullname_body = RequestBody.create(MediaType.parse("multipart/form-data"),fullname);
+        RequestBody phone_body = RequestBody.create(MediaType.parse("multipart/form-data"),phone);
+        RequestBody address_body = RequestBody.create(MediaType.parse("multipart/form-data"),address);
+
+
+
+    }
+
+
+
 }
